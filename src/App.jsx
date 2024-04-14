59f1c190cd92acae7c07146e1411b9ea76441413
M0H1K4N <nejer911@gmail.com>
2024-04-14 22:40:17 +0400

    mobile home done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import HomePage from './Components/Mobile/mobileHome'
-import { Route } from 'react-router'
+import HomePage from "./Components/Mobile/mobileHome";
+import MoonPage from './Components/Mobile/mobileMoon'
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <Router>
       <Routes>
-        <Route path='/' />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Moon" element={<MoonPage />} />
       </Routes>
     </Router>
-      
-        
-  
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Mobile/mobileHome.jsx
+++ src/Components/Mobile/mobileHome.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../../assets/shared/logo.svg";
+import HamBurger from "../../../assets/shared/icon-hamburger.svg";
+import "./Mobile.css";
 
 export default function mobileHome() {
   return (
-    <div>
-      <h1>hello from mobile Home</h1>
+    <div className="mainMobile">
+      <div className="mobileHeader">
+        <img className="logo" src={Logo} alt="logo" />
+        <img src={HamBurger} alt="logo" />
+      </div>
+      <div className="txts">
+        <h4 className="mobileH4">SO, YOU WANT TO TRAVEL TO</h4>
+        <h1 className="mobileH1">SPACE</h1>
+        <h3 className="mobileH3">
+          Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!
+        </h3>
+      </div>
+      <button className="mobileHomeButton">EXPLORE</button>
     </div>
-  )
+  );
 }
