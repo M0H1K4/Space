ab6d935ba7a8c2b0f6618aca6943ea26239935f2
M0H1K4N <nejer911@gmail.com>
2024-04-15 02:44:37 +0400

    add

--- src/Components/Mobile/NavBar.jsx
+++ src/Components/Mobile/NavBar.jsx
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import HamBurger from "../../../assets/shared/icon-hamburger.svg";
 import Logo from "../../../assets/shared/logo.svg";
 import "./Mobile.css";
 
-export default function NavBar({ isOpen, toggle }) {
+export default function NavBar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isActive ? "" : "hidden"}`}>
       <div className="mobileHeader">
         <img className="logo" src={Logo} alt="logo" />
         <img
           src={HamBurger}
           alt="hamburger"
           className="hamburger"
-          onClick={toggle} // Add onClick event handler here
+          onClick={toggleMenu}
         />
       </div>
-      <ul className="navbar-menu">
+      <ul className={`navbar-menu ${isActive ? "active" : ""}`}>
         <li className="navbar-item">
           <Link to="/" className="navbar-link">
-            Home
+            00 HOME
           </Link>
         </li>
         <li className="navbar-item">
           <Link to="/about" className="navbar-link">
-            About
+            01 DESTINATION
+          </Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/about" className="navbar-link">
+            02 CREW
+          </Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/about" className="navbar-link">
+            03 TTECHNOLOGY
           </Link>
         </li>
       </ul>
